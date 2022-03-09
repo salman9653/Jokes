4df4ba6a83fab12a54c8314dd87c34aae8a47900
Salman <salman9915189734@gmail.com>
2022-03-09 23:38:19 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const jokes = document.querySelector("#joke");
+const jokeBtn = document.querySelector('#jokeBtn');
+
+// Using Promisis
+// function generateJokes(){
+//     const setHeader = {
+//         headers : {
+//             Accept : "application/json"
+//         }
+//     };
+//     fetch('https://icanhazdadjoke.com/', setHeader)
+//     .then( res => res.json() )
+//     .then( data => joke.innerHTML = data.joke )
+//     .catch((error) => {
+//         console.log(error)
+//     })
+// }
+
+// jokeBtn.addEventListener('click', generateJokes);
+// generateJokes();
+
+
+
+//Using Async-Await
+const generateJokes = async () => {
+    try {
+
+        const setHeader = {
+            headers: {
+                Accept: "application/json"
+            }
+        };
+        const res = await fetch('https://icanhazdadjoke.com/', setHeader);
+        const data = await res.json();
+        joke.innerHTML = data.joke;
+
+    } catch (err) {
+        console.log(`the error is ${err}`);
+    }
+}
+
+
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
